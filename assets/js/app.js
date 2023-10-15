817470eb2a0b75baa086f3b63fc00dec37316343
G-Issa-11 <ghada.issa314@gmail.com>
2023-10-15 21:00:28 +0200

    color palette refactored

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,3 +50,18 @@ window.addEventListener("scroll", () => {
     backToTopButton.classList.remove("active");
   }
 });
+
+// header active
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
